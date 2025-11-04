5841aa8ff6ac3381f94450d4c5f1de182d2bdbc5
Siano <117085166+Sianooo@users.noreply.github.com>
2025-11-04 08:47:25 +0100

    Add files via upload

---
+++ Wyszukiwanie wartosci/Tabela/Tabela/Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace Tabela
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.min = new System.Windows.Forms.Button();
+            this.max = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.sortowanie = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.szukanie = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10});
+            this.dataGridView1.Location = new System.Drawing.Point(140, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(349, 52);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 30;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 30;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 30;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 30;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 30;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 30;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 30;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column8";
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 30;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Column9";
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 30;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Column10";
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 30;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(400, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "wypełnij";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // min
+            // 
+            this.min.Location = new System.Drawing.Point(178, 286);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(75, 23);
+            this.min.TabIndex = 2;
+            this.min.Text = "min";
+            this.min.UseVisualStyleBackColor = true;
+            this.min.Click += new System.EventHandler(this.min_Click);
+            // 
+            // max
+            // 
+            this.max.Location = new System.Drawing.Point(381, 286);
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(75, 23);
+            this.max.TabIndex = 3;
+            this.max.Text = "max";
+            this.max.UseVisualStyleBackColor = true;
+            this.max.Click += new System.EventHandler(this.max_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 318);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Najmniejszy element";
+            this.label1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(322, 315);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(167, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Visible = false;
+            // 
+            // sortowanie
+            // 
+            this.sortowanie.Location = new System.Drawing.Point(281, 286);
+            this.sortowanie.Name = "sortowanie";
+            this.sortowanie.Size = new System.Drawing.Size(75, 23);
+            this.sortowanie.TabIndex = 6;
+            this.sortowanie.Text = "sortowanie";
+            this.sortowanie.UseVisualStyleBackColor = true;
+            this.sortowanie.Click += new System.EventHandler(this.sortowanie_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7,
+            this.dataGridViewTextBoxColumn8,
+            this.dataGridViewTextBoxColumn9,
+            this.dataGridViewTextBoxColumn10});
+            this.dataGridView2.Location = new System.Drawing.Point(140, 92);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(349, 51);
+            this.dataGridView2.TabIndex = 7;
+            this.dataGridView2.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Column1";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 30;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Column2";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 30;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Column3";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 30;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Column4";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 30;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.HeaderText = "Column5";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.Width = 30;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.HeaderText = "Column6";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.Width = 30;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.HeaderText = "Column7";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.Width = 30;
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.HeaderText = "Column8";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            this.dataGridViewTextBoxColumn8.Width = 30;
+            // 
+            // dataGridViewTextBoxColumn9
+            // 
+            this.dataGridViewTextBoxColumn9.HeaderText = "Column9";
+            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
+            this.dataGridViewTextBoxColumn9.Width = 30;
+            // 
+            // dataGridViewTextBoxColumn10
+            // 
+            this.dataGridViewTextBoxColumn10.HeaderText = "Column10";
+            this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
+            this.dataGridViewTextBoxColumn10.Width = 30;
+            // 
+            // szukanie
+            // 
+            this.szukanie.Location = new System.Drawing.Point(115, 220);
+            this.szukanie.Name = "szukanie";
+            this.szukanie.Size = new System.Drawing.Size(110, 23);
+            this.szukanie.TabIndex = 8;
+            this.szukanie.Text = "szukanie elementu";
+            this.szukanie.UseVisualStyleBackColor = true;
+            this.szukanie.Click += new System.EventHandler(this.szukanie_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 368);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "podaj element ktorego szukasz";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(322, 361);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(167, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(241, 220);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Szukanie rekurencyjnie";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(497, 220);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Sortuj przez scalanie";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.szukanie);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.sortowanie);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.max);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Tabele";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button min;
+        private System.Windows.Forms.Button max;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button sortowanie;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
+        private System.Windows.Forms.Button szukanie;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Wyszukiwanie wartosci/Tabela/Tabela/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabela
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void sortuj(int[] tab)
+        {
+            int temp;
+            for (int i=0;i<10;i++)
+            {
+                for (int j=0;j<9;j++)
+                {
+                    if (tab[j] > tab[j+1])
+                    {
+                        temp = tab[j];
+                        tab[j]= tab[j+1];
+                        tab[j+1]= temp;
+                    }
+                }
+            }
+        }
+        private int szukaj(int[] tab,int szukana,int p,int k)
+        {
+            int srodek;
+           
+            while (p<=k) {
+                srodek =  (k+p)/ 2;
+                if (tab[srodek] == szukana)
+                {
+                    return srodek;
+                }
+                else
+                {
+                    if (szukana < tab[srodek])
+                    {
+                        k = srodek - 1;
+                    }
+                    else
+                    {
+                        p = srodek + 1;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        private int rekurencjeSzukanie(int[] tab, int szukana, int p, int k)
+        {
+            int srodek;
+            if (p > k)
+            {
+                return -1;
+            }
+            else
+            {
+                srodek = (k + p) / 2;
+                if (tab[srodek] == szukana)
+                {
+                    return srodek;
+                }
+                if (szukana < tab[srodek])
+                {
+                    return rekurencjeSzukanie(tab, szukana, p, srodek - 1);
+                }
+                if(szukana > tab[srodek])
+                {
+                    return rekurencjeSzukanie(tab, szukana, k, srodek + 1);
+                }
+            }
+            return -1;
+        }
+        int[] temp;
+        private void scalanie(int[] tab, int p, int k)
+        {
+            for (int i = p; i <= k; i++)
+            {
+                temp[i] = tab[i];
+            }
+
+            int srodek = p + (k - p) / 2;
+            int i1 = p;   
+            int i2 = srodek + 1; 
+            int i3 = p;
+
+            while (i1 <= srodek && i2 <= k)
+            {
+                if (temp[i1] <= temp[i2])
+                {
+                    tab[i3] = temp[i1];
+                    i1++;
+                }
+                else
+                {
+                    tab[i3] = temp[i2];
+                    i2++;
+                }
+                i3++;
+            }
+
+            while (i1 <= srodek)
+            {
+                tab[i3] = temp[i1];
+                i1++;
+                i3++;
+            }
+
+            while (i2 <= k)
+            {
+                tab[i3] = temp[i2];
+                i2++;
+                i3++;
+            }
+        }
+
+        private void sortowaniePrzezSkalanie(int[] tab,int p, int k)
+        {
+            if (p >= k) return;
+            int srodek = (k + p) / 2;
+            sortowaniePrzezSkalanie(tab, p, srodek);
+            sortowaniePrzezSkalanie(tab, srodek + 1, k);
+            scalanie(tab, p, k);
+        }
+
+        int[] tab;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            tab = new int[dataGridView1.Columns.Count];
+            for (int i=0;i<10;i++)
+            {
+                dataGridView1.Rows[0].Cells[i].Value = random.Next(1,101);
+                tab[i]=(int)dataGridView1.Rows[0].Cells[i].Value;
+            }
+            label1.Hide();
+            textBox1.Hide();
+            dataGridView2.Hide();
+        }
+
+        private void min_Click(object sender, EventArgs e)
+        {
+            int min = 100;
+            for (int i =0;i<10;i++)
+            {
+                if (min > tab[i])
+                {
+                    min = tab[i];
+                }
+            }
+            label1.Show();
+            textBox1.Show();
+            textBox1.Text = min.ToString();
+            
+            
+        }
+
+        private void max_Click(object sender, EventArgs e)
+        {
+            int max = tab[0];
+            for (int i=0;i<10;i++)
+            {
+                if (max < tab[i])
+                {
+                    max=tab[i];
+                }
+            }
+            label1.Text = "Największy element: ";
+            label1.Show();
+            textBox1.Show();
+            textBox1.Text = max.ToString();
+        }
+
+        private void sortowanie_Click(object sender, EventArgs e)
+        {
+            sortuj(tab);
+            for (int i=0;i<10; i++)
+            {
+                dataGridView2.Rows[0].Cells[i].Value = tab[i];
+            }
+            label1.Hide();
+            textBox1.Hide();
+            dataGridView2.Show();
+
+            
+        }
+
+        private void szukanie_Click(object sender, EventArgs e)
+        {
+            int szukany = int.Parse(textBox2.Text);
+            int w= szukaj(tab,szukany,0,tab.Length -1);
+            
+                
+            label1.Show();
+            textBox1.Show();
+            if(w==-1)
+            {
+                textBox1.Text = "Nie ma takiej liczby w tablicy";
+            }
+            else
+            {
+                textBox1.Text = w.ToString();
+                label1.Text = "Szukany element znajduje sie na pozycji";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int szukany = int.Parse(textBox2.Text);
+            int w = rekurencjeSzukanie(tab, szukany, 0, tab.Length - 1);
+
+
+            label1.Show();
+            textBox1.Show();
+            if (w == -1)
+            {
+                textBox1.Text = "Nie ma takiej liczby w tablicy";
+            }
+            else
+            {
+                textBox1.Text = w.ToString();
+                label1.Text = "Szukany element znajduje sie na pozycji";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            temp = new int[tab.Length];
+            sortowaniePrzezSkalanie(tab, 0, tab.Length - 1);
+            for (int i = 0; i < tab.Length; i++)
+            {
+                dataGridView2.Rows[0].Cells[i].Value = tab[i];
+            }
+            label1.Hide();
+            textBox1.Hide();
+            dataGridView2.Show();
+        }
+    }
+}
